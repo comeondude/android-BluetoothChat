77d57a59b4a387b3a5bb9eb13ee1b5f09c883dbb
Reaz Murshed <masud@Reazs-MacBook-Pro.local>
2019-04-11 13:16:14 -0400

    Refactoring

--- Application/src/main/java/com/example/android/bluetoothchat/Constants.java
+++ Application/src/main/java/com/example/android/bluetoothchat/Constants.java
@@ -31,5 +31,4 @@ public interface Constants {
     // Key names received from the BluetoothChatService Handler
     public static final String DEVICE_NAME = "device_name";
     public static final String TOAST = "toast";
-
 }
